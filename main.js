86dca424ec3aad8245594b24e6a86e50ec33afa0
Arttu Hasa <arttuhasa@gmail.com>
2025-09-30 15:05:59 +0300

    lampotilamuunnin

---
+++ main.js
@@ -0,0 +1,49 @@
+let muunnin = document.forms['lisays'];
+muunnin.addEventListener('submit', kaannos);
+otsikko = document.getElementById("tulos");
+var valinta = document.getElementById("astevalinta")
+
+
+
+function kaannos(event){
+    event.preventDefault();
+    if(document.querySelector('input[type = radio]:checked') == null){
+      alert('valitse desimaalit');
+        return;
+    }
+    var desimaalit = document.querySelector('input[type = radio]:checked').value
+    var value = valinta.value;
+    let luku = document.querySelector('input[type="text"]').value;
+    if(luku.length < 1){
+        alert('syötä luku');
+        return;
+    }
+    if(isNaN(luku)){
+        alert('Syötä vain numeroita');
+        return;
+    }
+    if(value == "Celcius"){
+      luku = luku *1.8;
+        luku = luku +32;
+    } else if(value == 'Farenheit'){
+      luku = luku -32;
+      luku = luku /1.8
+    }
+    luku = String(luku)
+    if(luku.includes(".")){
+      loppu = luku.split(".")
+      loppu[1] = loppu[1].slice(0,desimaalit)
+      otsikko.innerHTML = "Tulos:" + " " + loppu[0] + "." + loppu[1];
+    } else{
+      otsikko.innerHTML = "Tulos:" + " " + luku;
+    }
+
+
+    
+    
+
+
+
+
+}
+
